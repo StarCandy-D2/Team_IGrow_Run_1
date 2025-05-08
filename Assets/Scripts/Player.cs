d22c9680d91d1aa9dbdd63cc03dfd384321c1782
Yusang-1 <tearless25hg@gmail.com>
2025-05-08 12:52:30 +0900

    Update : sliding, 점프 제한
    - 슬라이딩과 점프 제한 기능 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     Rigidbody2D rigid;
     [SerializeField] float runSpeed;
     [SerializeField] float jumpPower;
+    [SerializeField] int jumpCount = 0;
 
     private void Start()
     {
@@ -16,6 +17,7 @@ public class Player : MonoBehaviour
     private void Update()
     {
         RunAndJump();
+        Sliding();
     }
 
     void RunAndJump()
@@ -25,9 +27,31 @@ public class Player : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump"))
         {
-            vec.y = jumpPower;
+            if(jumpCount < 2)
+            {
+                jumpCount++;
+                vec.y = jumpPower;
+            }
         }
 
         rigid.velocity = vec;
+    }
+
+    void Sliding()
+    {
+        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.E))
+        {
+            Vector2 vec = rigid.velocity;
+            vec.y = -jumpPower;
+            rigid.velocity = vec;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Ground")
+        {
+            jumpCount = 0;
+        }
     }
 }
